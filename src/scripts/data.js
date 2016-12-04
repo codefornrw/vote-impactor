d23a5e8d5b65b08dc357612689ae3c25890709aa
Simon Wörpel <simon.woerpel@medienrevolte.de>
2016-12-04 12:10:20 +0100

    Use gh-pages branch for build.

--- src/scripts/data.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  districtsData: require('dsv!../data/wahlkreise.csv'),
-  nrwData: require('dsv!../data/nrw.csv')[0]
-}
--- src/scripts/events.js
+++
@@ -1,6 +0,0 @@
-riot.EVT = {
-  showDistrict: 'show_district',
-  clear: 'clear'
-}
-
-riot.control = riot.observable()
